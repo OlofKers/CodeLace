c5b1e599134a749f9ec6a5b5a7929068d3745f2e
OlofKers <kerssemeeckers.olaf@gmail.com>
2026-03-20 00:02:02 +0100

    removed deprecated arguments from MongoDB functioncall

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,7 @@ app.get("/", (req, res) => {
 })
 
 // Connect to Database
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error(err))
 
